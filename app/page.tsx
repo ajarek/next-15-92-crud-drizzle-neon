04ed339e4e6612189f2947dea9c37993fba02640
Ajarek <ajarek@poczta.onet.pl>
2025-07-22 00:55:07 +0200

    added table Neon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-2xl font-semibold '>Home</h1>
+    </div>
   )
 }
 
